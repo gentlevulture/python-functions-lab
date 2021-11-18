594685a0578cd7a47d6713725f423425ec0ac56e
gentlevulture <novemberboughs@gmail.com>
2021-11-17 23:27:18 -0800

    chllng2

--- python-functions.py
+++ python-functions.py
@@ -1,8 +1,27 @@
-def sum_to(num):
-  sum = 0
-  for n in range(1, num + 1):
-    sum += n
-  return sum
+# Challenge 1
 
-print(sum_to(6))
-print(sum_to(10))
+def sum_to(num):
+  total = 0
+  for n in range(1, num + 1):
+    total += n
+  return total
+
+print(sum_to(6)) # returns 21
+print(sum_to(10)) # returns 55
+
+# Challenge 2
+
+def largest(lst):
+  return max(lst)
+
+print(largest([1, 2, 3, 4, 0]))  # returns 4
+print(largest([10, 4, 2, 231, 91, 54]))  # returns 231
+
+# alt947
+# def largest(nums):
+#   nums.sort()
+#   return nums[-1]
+
+# Challenge 3
+
+# Challenge 4
